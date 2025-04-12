7d16b25793110b0a25e4a93ce73801e762b04ec3
Ahmad Zo-ElFakkar <ahmadzoelfakkar@gmail.com>
2025-04-13 00:44:47 +0200

    small framework for the project

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello")
+import numpy as np
+import pandas as pd
+
+
+# Read the CSV files
+data_1 = pd.read_csv('Acquired Tech Companies.csv')
+data_2 = pd.read_csv('Acquiring Tech Companies.csv')
+data_3 = pd.read_csv('Acquisitions.csv')
+data_4 = pd.read_csv('Founders and Board Members.csv')
